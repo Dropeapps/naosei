0cde8fc0f667867157f6e814a06aed92df8aa511
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-26 02:46:17 -0300

    Update input.py

--- input.py
+++ input.py
@@ -1,17 +1,28 @@
 import streamlit as st
+import hashlib
 
-# Criando uma função para o login
-def login():
-    st.title("Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type='password')
-    if st.button("Entrar"):
-        if username == "admin" and password == "admin":
-            st.success("Login realizado com sucesso!")
-        else:
-            st.error("Usuário ou senha inválidos")
+login = st.text_input("Insira o login")
+senha = st.text_input("Insira a senha", type='password')
+cadastrar = st.button("Cadastrar")
 
-# Adicionando a função no layout da página
-st.set_page_config(page_title="Plataforma de Login", page_icon=":guardsman:", layout="wide")
-login()
+def criptografar(senha):
+    senha_criptografada = hashlib.sha256(senha.encode()).hexdigest()
+    return senha_criptografada
+
+if cadastrar:
+    senha_criptografada = criptografar(senha)
+    usuarios = {}
+    usuarios[login] = senha_criptografada
+    st.success("Usuário cadastrado com sucesso!")
+
+login_entrar = st.text_input("Insira o login")
+senha_entrar = st.text_input("Insira a senha", type='password')
+entrar = st.button("Entrar")
+
+if entrar:
+    senha_digitada = criptografar(senha_entrar)
+    if login_entrar in usuarios and usuarios[login_entrar] == senha_digitada:
+        st.success("Login realizado com sucesso!")
+    else:
+
 
